560e965f1557d31d575da467041a72a2f516b9db
Yan <yqp_001@163.com>
2019-08-11 12:32:49 +0800

    目录，catalog

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/simple_counter.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,53 +12,29 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter 练习场'),
+      home: Catalog(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class Catalog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      appBar: AppBar(title: Text('目录')),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('计数器'),
+            subtitle: Text('计数器最简单的实现'),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return SimpleCounter(title: '简单计数器');
+              }));
+            },
+          ),
+          Divider(height: 0),
+        ],
       ),
     );
   }
